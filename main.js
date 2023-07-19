80c41fc07c8c23d57fd753c9f5db45685c98ecca
paulviville <paul.viville@etu.unistra.fr>
2023-07-19 14:14:47 +0200

    basic volumes skinning

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ import Renderer from './CMapJS/Rendering/Renderer.js';
 import * as THREE from './CMapJS/Libs/three.module.js';
 import { OrbitControls } from './CMapJS/Libs/OrbitsControls.js';
 import Skeleton, {Key, SkeletonRenderer } from './Skeleton.js';
-import SkeletonLBS, {KeyLBS, SkeletonRendererLBS } from './SkeletonLBS.js';
-// import DualQuaternion from './DualQuaternion.js';
 import {DualQuaternion} from './DualQuaternion.js';
+import CMap3 from './CMapJS/CMap/CMap3.js'
+
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xeeeeee);
@@ -42,63 +42,27 @@ const worldY = new THREE.Vector3(0, 1, 0);
 
 
 const translation = new THREE.Quaternion(0, 0.1, 0, 0);
-const translation1 = new THREE.Quaternion(0, 0.15, 0, 0);
-const translationLBS = new THREE.Vector3(0, 0.1, 0);
-const translation1LBS = new THREE.Vector3(0, 0.15, 0);
+const translation1 = new THREE.Quaternion(0, 0.1, 0, 0);
+
 
 const rotation = new THREE.Quaternion().setFromAxisAngle(worldY, Math.PI / 3);
 const transform = DualQuaternion.setFromRotationTranslation(rotation.clone(), translation.clone());
 transform.normalize();
 const key0 = new Key(0, transform);
 
-let mR = new THREE.Matrix4().makeRotationFromQuaternion(rotation);
-let mT = new THREE.Matrix4().makeTranslation(translationLBS.x, translationLBS.y, translationLBS.z);
-const keyLBS0 = new KeyLBS(0, mR.clone().multiply(mT));
 
-const rotation1 = new THREE.Quaternion().setFromAxisAngle(worldUp, Math.PI / 3);
-// const rotation1 = new THREE.Quaternion().setFromAxisAngle(worldUp, 0);
+// const rotation1 = new THREE.Quaternion().setFromAxisAngle(worldUp, Math.PI / 3);
+const rotation1 = new THREE.Quaternion().setFromAxisAngle(worldUp, 0);
 const transform1 = DualQuaternion.setFromRotationTranslation(rotation1.clone(), translation1.clone());
 transform1.normalize();
 const key1 = new Key(100, transform1);
 
-rotation1
-let mR1 = new THREE.Matrix4().makeRotationFromQuaternion(rotation1);
-let mT1 = new THREE.Matrix4().makeTranslation(translation1LBS.x, translation1LBS.y, translation1LBS.z);
-const keyLBS1 = new KeyLBS(100, mR1.clone().multiply(mT1));
 
-const translationroot = new THREE.Quaternion(-0.25, 0, 0, 0);
+const translationroot = new THREE.Quaternion(0, 0, 0, 0);
 const transformRoot = DualQuaternion.setFromRotationTranslation(new THREE.Quaternion, translationroot)
 const keyroot = new Key(100, transformRoot);
 
 
-const translationLBSroot = new THREE.Vector3(0.25, 0, 0);
-let mTroot = new THREE.Matrix4().makeTranslation(translationLBSroot.x, translationLBSroot.y, translationLBSroot.z);
-const keyLBSroot = new KeyLBS(100, mTroot);
-
-
-const skeletonLBS = new SkeletonLBS;
-const rootLBS = skeletonLBS.newBone("root");
-skeletonLBS.addKey(rootLBS, keyLBSroot);
-const boneLBS0 = skeletonLBS.newBone();
-skeletonLBS.setParent(boneLBS0, rootLBS);
-skeletonLBS.addKey(boneLBS0, keyLBS0);
-// skeletonLBS.addKey(boneLBS0, keyLBS1);
-const boneLBS1 = skeletonLBS.newBone();
-skeletonLBS.setParent(boneLBS1, boneLBS0);
-skeletonLBS.addKey(boneLBS1, keyLBS0);
-// skeletonLBS.addKey(boneLBS1, keyLBS1);
-const boneLBS2 = skeletonLBS.newBone();
-skeletonLBS.setParent(boneLBS2, boneLBS1);
-skeletonLBS.addKey(boneLBS2, keyLBS0);
-skeletonLBS.addKey(boneLBS2, keyLBS1);
-const boneLBS3 = skeletonLBS.newBone();
-skeletonLBS.setParent(boneLBS3, boneLBS2);
-skeletonLBS.addKey(boneLBS3, keyLBS0);
-// skeletonLBS.addKey(boneLBS3, keyLBS1);
-
-skeletonLBS.addKey(boneLBS0, keyLBS1);
-skeletonLBS.addKey(boneLBS1, keyLBS1);
-skeletonLBS.addKey(boneLBS3, keyLBS1);
 
 
 const skeleton = new Skeleton;
@@ -107,11 +71,9 @@ skeleton.addKey(root, keyroot);
 const bone0 = skeleton.newBone();
 skeleton.setParent(bone0, root);
 skeleton.addKey(bone0, key0);
-// skeleton.addKey(bone0, key1);
 const bone1 = skeleton.newBone();
 skeleton.setParent(bone1, bone0);
 skeleton.addKey(bone1, key0);
-// skeleton.addKey(bone1, key1);
 const bone2 = skeleton.newBone();
 skeleton.setParent(bone2, bone1);
 skeleton.addKey(bone2, key0);
@@ -119,7 +81,6 @@ skeleton.addKey(bone2, key1);
 const bone3 = skeleton.newBone();
 skeleton.setParent(bone3, bone2);
 skeleton.addKey(bone3, key0);
-// skeleton.addKey(bone3, key1);
 
 skeleton.addKey(bone0, key1);
 skeleton.addKey(bone1, key1);
@@ -136,15 +97,8 @@ sRenderer.createEdges();
 scene.add(sRenderer.vertices)
 scene.add(sRenderer.edges)
 
-skeletonLBS.computeWorldTransforms(0);
-skeletonLBS.setBindTransforms();
-// skeletonLBS.computeOffsets();
 
-const sRendererLBS = new SkeletonRendererLBS(skeletonLBS);
-sRendererLBS.createVertices();
-sRendererLBS.createEdges();
-scene.add(sRendererLBS.vertices)
-scene.add(sRendererLBS.edges)
+
 
 const skin = new IncidenceGraph;
 skin.createEmbedding(skin.vertex);
@@ -152,29 +106,31 @@ const skinPos = skin.addAttribute(skin.vertex, "position");
 const skinBind = skin.addAttribute(skin.vertex, "bind");
 const skinWeights = skin.addAttribute(skin.vertex, "weights");
 
-const skinLBS = new IncidenceGraph;
-skinLBS.createEmbedding(skinLBS.vertex);
-const skinLBSPos = skinLBS.addAttribute(skinLBS.vertex, "position");
-const skinLBSBind = skinLBS.addAttribute(skinLBS.vertex, "bind");
-const skinLBSWeights = skinLBS.addAttribute(skinLBS.vertex, "weights");
 
+// const weights = [
+// 	[{b: 0, w: 0.5}, {b: 1, w: 0.5}],
+// 	[{b: 0, w: 0.25}, {b: 1, w: 0.5}, {b: 2, w: 0.25}],
+// 	[{b: 1, w: 0.5}, {b: 2, w: 0.5}],
+// 	[{b: 1, w: 0.25}, {b: 2, w: 0.5}, {b: 3, w: 0.25}],
+// 	[{b: 2, w: 0.5}, {b: 3, w: 0.5}],
+// 	[{b: 2, w: 0.25}, {b: 3, w: 0.5}, {b: 4, w: 0.25}],
+// 	[{b: 3, w: 0.5}, {b: 4, w: 0.5}],
+// 	[{b: 3, w: 0.25}, {b: 4, w: 0.75}],
+// 	[{b: 4, w: 1}]
+// ]
 
 const weights = [
 	[{b: 0, w: 0.5}, {b: 1, w: 0.5}],
-	[{b: 0, w: 0.25}, {b: 1, w: 0.5}, {b: 2, w: 0.25}],
 	[{b: 1, w: 0.5}, {b: 2, w: 0.5}],
-	[{b: 1, w: 0.25}, {b: 2, w: 0.5}, {b: 3, w: 0.25}],
 	[{b: 2, w: 0.5}, {b: 3, w: 0.5}],
-	[{b: 2, w: 0.25}, {b: 3, w: 0.5}, {b: 4, w: 0.25}],
 	[{b: 3, w: 0.5}, {b: 4, w: 0.5}],
-	[{b: 3, w: 0.25}, {b: 4, w: 0.75}],
 	[{b: 4, w: 1}]
 ]
 
-const upVector = new THREE.Vector3(0, 0.05, 0);
-const radiusVector = new THREE.Vector3(0.075, 0, 0);
+const upVector = new THREE.Vector3(0, 0.1, 0);
+const radiusVector = new THREE.Vector3(0.05, 0, 0.05);
 
-const nbVerts = 10;
+const nbVerts = 4;
 const angle = 2*Math.PI/nbVerts;
 
 for(let w = 0; w < weights.length; ++w) {
@@ -187,28 +143,9 @@ for(let w = 0; w < weights.length; ++w) {
 		if(i == nbVerts - 1) skin.addEdge(id, id - i);
 		if(w > 0) skin.addEdge(id, id - nbVerts);
 		
-		skinPos[id] = tempVector.clone().addScaledVector(translationLBSroot, -1);
+		skinPos[id] = tempVector.clone();
 		skinBind[id] = skinPos[id].clone();
 		skinWeights[id] = weights[w];
-		
-		tempVector.applyAxisAngle(worldY, angle);
-	}
-}
-
-/// LBS
-for(let w = 0; w < weights.length; ++w) {
-	const tempVector = radiusVector.clone();
-	tempVector.addScaledVector(upVector, w);
-
-	for(let i = 0; i < nbVerts; ++i){
-		let id = skinLBS.addVertex();
-		if(i != 0) skinLBS.addEdge(id, id - 1);
-		if(i == nbVerts - 1) skinLBS.addEdge(id, id - i);
-		if(w > 0) skinLBS.addEdge(id, id - nbVerts);
-		
-		skinLBSPos[id] = tempVector.clone().add(translationLBSroot);
-		skinLBSBind[id] = skinLBSPos[id].clone();
-		skinLBSWeights[id] = weights[w];
 		
 		tempVector.applyAxisAngle(worldY, angle);
 	}
@@ -220,11 +157,128 @@ skinRenderer.vertices.addTo(scene)
 skinRenderer.edges.create({color: new THREE.Color(0x550000)});
 skinRenderer.edges.addTo(scene)
 
-const skinLBSRenderer = new Renderer(skinLBS);
-skinLBSRenderer.vertices.create({color: new THREE.Color(0x00FF00)});
-skinLBSRenderer.vertices.addTo(scene)
-skinLBSRenderer.edges.create({color: new THREE.Color(0x005500)});
-skinLBSRenderer.edges.addTo(scene)
+
+
+
+
+
+const hexMesh = new CMap3;
+const dh0 = hexMesh.addPrism(4, false);
+const dh1 = hexMesh.addPrism(4, false);
+const dh2 = hexMesh.addPrism(4, false);
+const dh3 = hexMesh.addPrism(4, false);
+
+const dh0_ = hexMesh.phi([2,1,1,2], dh0);
+const dh1_ = hexMesh.phi([2,1,1,2], dh1);
+const dh2_ = hexMesh.phi([2,1,1,2], dh2);
+const dh3_ = hexMesh.phi([2,1,1,2], dh3);
+
+let d0 = dh0; let d0_ = dh0_;
+let d1 = dh1; let d1_ = dh1_;
+let d2 = dh2; let d2_ = dh2_;
+let d3 = dh3; let d3_ = dh3_;
+
+for(let i = 0; i < 4; ++i) {
+	hexMesh.sewPhi3(d0_, d1);
+	hexMesh.sewPhi3(d1_, d2);
+	hexMesh.sewPhi3(d2_, d3);
+
+	d0_ = hexMesh.phi1[d0_];
+	d1_ = hexMesh.phi1[d1_];
+	d2_ = hexMesh.phi1[d2_];
+	d1 = hexMesh.phi_1[d1];
+	d2 = hexMesh.phi_1[d2];
+	d3 = hexMesh.phi_1[d3];
+}
+
+hexMesh.close();
+hexMesh.setEmbeddings(hexMesh.vertex);
+
+const hexPos = hexMesh.addAttribute(hexMesh.vertex, "position");
+const hexBind = hexMesh.addAttribute(hexMesh.vertex, "bind");
+const hexWeight =  hexMesh.addAttribute(hexMesh.vertex, "position");
+
+d0 = dh0; d1 = dh1; d2 = dh2; d3 = dh3; d3_ = hexMesh.phi1[dh3_];
+hexPos[hexMesh.cell(hexMesh.vertex, d0)] = new THREE.Vector3(-0.05, 0, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d1)] = new THREE.Vector3(-0.05, 0.1, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d2)] = new THREE.Vector3(-0.05, 0.2, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3)] = new THREE.Vector3(-0.05, 0.3, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3_)] = new THREE.Vector3(-0.05, 0.4, -0.05)
+
+hexWeight[hexMesh.cell(hexMesh.vertex, d0)] = [{b: 0, w: 0.5}, {b: 1, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d1)] = [{b: 1, w: 0.5}, {b: 2, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d2)] = [{b: 2, w: 0.5}, {b: 3, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3)] = [{b: 3, w: 0.5}, {b: 4, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3_)] = [{b: 4, w: 1}];
+
+
+d0 = hexMesh.phi1[d0]; d1 = hexMesh.phi1[d1]; d2 = hexMesh.phi1[d2];
+d3 = hexMesh.phi1[d3]; d3_ = hexMesh.phi_1[d3_];
+hexPos[hexMesh.cell(hexMesh.vertex, d0)] = new THREE.Vector3(0.05, 0, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d1)] = new THREE.Vector3(0.05, 0.1, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d2)] = new THREE.Vector3(0.05, 0.2, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3)] = new THREE.Vector3(0.05, 0.3, -0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3_)] = new THREE.Vector3(0.05, 0.4, -0.05)
+
+hexWeight[hexMesh.cell(hexMesh.vertex, d0)] = [{b: 0, w: 0.5}, {b: 1, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d1)] = [{b: 1, w: 0.5}, {b: 2, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d2)] = [{b: 2, w: 0.5}, {b: 3, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3)] = [{b: 3, w: 0.5}, {b: 4, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3_)] = [{b: 4, w: 1}];
+
+
+d0 = hexMesh.phi1[d0]; d1 = hexMesh.phi1[d1]; d2 = hexMesh.phi1[d2];
+d3 = hexMesh.phi1[d3]; d3_ = hexMesh.phi_1[d3_];
+hexPos[hexMesh.cell(hexMesh.vertex, d0)] = new THREE.Vector3(0.05, 0, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d1)] = new THREE.Vector3(0.05, 0.1, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d2)] = new THREE.Vector3(0.05, 0.2, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3)] = new THREE.Vector3(0.05, 0.3, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3_)] = new THREE.Vector3(0.05, 0.4, 0.05)
+
+hexWeight[hexMesh.cell(hexMesh.vertex, d0)] = [{b: 0, w: 0.5}, {b: 1, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d1)] = [{b: 1, w: 0.5}, {b: 2, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d2)] = [{b: 2, w: 0.5}, {b: 3, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3)] = [{b: 3, w: 0.5}, {b: 4, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3_)] = [{b: 4, w: 1}];
+
+
+d0 = hexMesh.phi1[d0]; d1 = hexMesh.phi1[d1]; d2 = hexMesh.phi1[d2];
+d3 = hexMesh.phi1[d3]; d3_ = hexMesh.phi_1[d3_];
+hexPos[hexMesh.cell(hexMesh.vertex, d0)] = new THREE.Vector3(-0.05, 0, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d1)] = new THREE.Vector3(-0.05, 0.1, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d2)] = new THREE.Vector3(-0.05, 0.2, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3)] = new THREE.Vector3(-0.05, 0.3, 0.05)
+hexPos[hexMesh.cell(hexMesh.vertex, d3_)] = new THREE.Vector3(-0.05, 0.4, 0.05)
+
+hexWeight[hexMesh.cell(hexMesh.vertex, d0)] = [{b: 0, w: 0.5}, {b: 1, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d1)] = [{b: 1, w: 0.5}, {b: 2, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d2)] = [{b: 2, w: 0.5}, {b: 3, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3)] = [{b: 3, w: 0.5}, {b: 4, w: 0.5}];
+hexWeight[hexMesh.cell(hexMesh.vertex, d3_)] = [{b: 4, w: 1}];
+
+// hexMesh.foreachDart(d => {
+// 	console.log(d, hexMesh.phi1[d], hexMesh.phi2[d], hexMesh.phi3[d])
+// })
+
+hexMesh.foreach(hexMesh.vertex, vd => {
+	const vid = hexMesh.cell(hexMesh.vertex, vd)
+	hexBind[vid] = hexPos[vid].clone();	
+})
+
+console.log(hexMesh.nbCells(hexMesh.vertex))
+console.log(hexMesh.nbCells(hexMesh.volume))
+console.log(hexMesh.nbCells(hexMesh.edge))
+
+
+
+const hexRenderer = new Renderer(hexMesh);
+hexRenderer.vertices.create();
+hexRenderer.vertices.addTo(scene)
+hexRenderer.edges.create();
+hexRenderer.edges.addTo(scene)
+hexRenderer.volumes.create({color: 0x7777BB}).rescale(0.9);
+hexRenderer.volumes.addTo(scene)
+
 
 
 const grid = new THREE.GridHelper(1, 10)
@@ -232,19 +286,7 @@ const grid2 = new THREE.GridHelper(1, 10)
 grid2.lookAt(worldY)
 scene.add(grid)
 scene.add(grid2)
-window.checkLocals = function() 
-{
-	skeleton.foreachBone(b => {
-		console.log(skeleton.getLocalTransform(b));
-	});
-}
 
-window.checkworlds = function() 
-{
-	skeleton.foreachBone(b => {
-		console.log(skeleton.getLocalTransform(b));
-	});
-}
 
 window.updateRenderer = function(t) {
 	sRenderer.computePositions(t);
@@ -260,13 +302,6 @@ function update (t)
 	sRenderer.updateVertices();
 	sRenderer.updateEdges();
 
-	sRendererLBS.computePositions(s);
-	skeletonLBS.computeOffsets()
-	sRendererLBS.updateVertices();
-	sRendererLBS.updateEdges();
-
-	let dqId = new DualQuaternion;
-	// console.log(dqId)
 	skin.foreach(skin.vertex, v => {
 		let pb = skinBind[v].clone();
 		let dqBlend = new DualQuaternion(new THREE.Quaternion(0,0,0,0), new THREE.Quaternion(0,0,0,0));
@@ -285,18 +320,24 @@ function update (t)
 	skinRenderer.vertices.update();
 	skinRenderer.edges.update();
 
-	skinLBS.foreach(skinLBS.vertex, v => {
-		let p0 = new THREE.Vector3();
-		let pb = skinLBSBind[v].clone();
-		for(let w = 0; w < skinLBSWeights[v].length; ++w) {
-			let b = skinLBSWeights[v][w];
-			let off = skeletonLBS.getOffset(b.b);
-			p0.addScaledVector(pb.clone().applyMatrix4(off), b.w);
+	hexMesh.foreach(hexMesh.vertex, vd => {
+		const vid = hexMesh.cell(hexMesh.vertex, vd);
+
+		const dqBlend = new DualQuaternion(new THREE.Quaternion(0,0,0,0), new THREE.Quaternion(0,0,0,0));
+		for(let w = 0; w < hexWeight[vid].length; ++w) {
+			let b = hexWeight[vid][w];
+			let off = skeleton.getOffset(b.b);
+			dqBlend.addScaledDualQuaternion(off, b.w);
 		}
-		skinLBSPos[v].copy(p0);
-	})
-	skinLBSRenderer.vertices.update();
-	skinLBSRenderer.edges.update();
+		dqBlend.normalize();
+		const pdq = DualQuaternion.setFromTranslation(hexBind[vid]);
+		pdq.multiplyDualQuaternions(dqBlend, pdq)
+		
+		hexPos[vid].copy(pdq.transform(new THREE.Vector3))
+	});
+
+	hexRenderer.volumes.update()
+	hexRenderer.volumes.rescale(0.9)
 }
 
 function render()
